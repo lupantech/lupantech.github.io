2f66b661b3545b9d5be99794823f82e3c8716212
lupantech <lupantech@gmail.com>
2024-10-10 11:38:22 -0700

    update

---
+++ push_github.sh
@@ -0,0 +1,4 @@
+cd /Users/panlu/Library/Mobile\ Documents/com~apple~CloudDocs/lupantech.github.io
+git add -A
+git commit -m "update"
+git push
--- push_github_notes.sh
+++
@@ -1,4 +0,0 @@
-cd /Users/panlu/Desktop/Research_Notes
-git add -A
-git commit -m "update"
-git push
